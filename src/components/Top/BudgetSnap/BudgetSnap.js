69e7306d9f302953a47b1dc2d7f39f73cc0f151a
Ed Wilgosz <EdWilgosz@gmail.com>
2020-01-11 10:27:48 -0500

    Handle BudgetSnap validation

--- src/components/Top/BudgetSnap/BudgetSnap.js
+++ src/components/Top/BudgetSnap/BudgetSnap.js
@@ -8,8 +8,8 @@ const BudgetSnap = props => {
     const state = useStore()[0];
     const spentValue = FORMAT_NUMBER(state.spentValue);
     const toSpendValue = FORMAT_NUMBER(state.toSpendValue);
-    const spentPerc = state.spentPerc;
-    const toSpendPerc = state.toSpendPerc;
+    const spentPerc = state.spentPerc < 1 ? '--' : state.spentPerc > 100 ? '100' : state.spentPerc;
+    const toSpendPerc = state.toSpendPerc > 0 ? state.toSpendPerc : '--';
 
     let type = props.type === 'spent';
     let contType = [type ? classes.Spent : classes.ToSpend, classes.Cont].join(' ');
